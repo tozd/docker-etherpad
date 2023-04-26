8efaa520cc3dc8e093391b666ce8147a197952bf
Mitar <mitar.git@tnode.com>
2023-04-27 01:32:59 +0200

    Add test.

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "Running Docker image"
+docker run -d --name test --rm -p 9001:9001 "${CI_REGISTRY_IMAGE}:${TAG}" || exit 1
+
+echo "Sleeping"
+sleep 10
+
+echo "Testing"
+curl -s http://docker:9001 | grep -q '<title>Etherpad</title>'
+result=$?
+
+echo "Stopping Docker image"
+docker stop test
+
+exit "$result"
