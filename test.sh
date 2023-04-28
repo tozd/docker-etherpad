51f851a68f8c6112796716cceb97bac59d59dcd8
Mitar <mitar.git@tnode.com>
2023-04-28 15:14:53 +0200

    Improve script.

--- test.sh
+++ test.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
+set -e
+
+cleanup() {
+  echo "Stopping Docker image"
+  docker stop test
+}
+
 echo "Running Docker image"
-docker run -d --name test --rm -p 9001:9001 "${CI_REGISTRY_IMAGE}:${TAG}" || exit 1
+docker run -d --name test --rm -p 9001:9001 "${CI_REGISTRY_IMAGE}:${TAG}"
+trap cleanup EXIT
 
 echo "Sleeping"
 sleep 10
 
 echo "Testing"
 wget -q -O - http://docker:9001 | grep -q '<title>Etherpad</title>'
-result=$?
-
-echo "Stopping Docker image"
-docker stop test || exit 2
-
-exit "$result"
